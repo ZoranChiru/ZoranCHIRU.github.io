a427d9d58a361c8ea0ae7b1ad246c90f86d15532
Zoran CHIRU <zoranchirupro@gmail.com>
2026-03-13 15:57:05 +0100

    Create files

---
+++ main.js
@@ -0,0 +1,8 @@
+jeu()
+
+// let inputEcrit = document.getElementById("mot_a_ecrire")
+// console.log(inputEcrit)
+
+// let btnValider = document.getElementById("btn_valider")
+// console.log(btnValider)
+
